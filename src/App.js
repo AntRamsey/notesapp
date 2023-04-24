16fb4f4c445d18d86342864e7949c9b999757c2a
AntRamsey <antwoneramsey@gmail.com>
2023-04-24 07:07:24 -0500

    Adding add note functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { v4 as uuid } from 'uuid';
 import { List, Input, Button } from 'antd';
 import { createNote as CreateNote } from './graphql/mutations';
 
+const CLIENT_ID = uuid()
+
 const initialState = {
   notes: [],
   loading: true,
@@ -17,6 +19,12 @@ const reducer = (state, action) => {
   switch(action.type) {
     case 'SET_NOTES':
       return { ...state, notes: action.notes, loading: false };
+    case 'ADD_NOTE':
+      return { ...state, notes: [action.note, ...state.notes]}
+    case 'RESET_FORM':
+      return { ...state, form: initialState.form }
+    case 'SET_INPUT':
+      return { ...state, form: { ...state.form, [action.name]: action.value } }
     case 'ERROR':
       return { ...state, loading: false, error: true };
     default:
@@ -24,12 +32,10 @@ const reducer = (state, action) => {
   }
 };
 
-const CLIENT_ID = uuid()
-
 const App = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const fetchNotes = async() => {
+  const fetchNotes = async () => {
     try {
       const notesData = await API.graphql({
         query: listNotes
@@ -40,6 +46,32 @@ const App = () => {
       dispatch({ type: 'ERROR' });
     }
   };
+
+  const createNote = async () => {
+    const { form } = state // destructuring - form element out of state
+
+    if (!form.name || !form.description) {
+       return alert('please enter a name and description');
+    }
+
+    const note = { ...form, clientId: CLIENT_ID, completed: false, id: uuid() };
+    dispatch({ type: 'ADD_NOTE', note });
+    dispatch({ type: 'RESET_FORM' });
+
+    try {
+      await API.graphql({
+        query: CreateNote,
+        variables: { input: note }
+      })
+      console.log('successfully created note!')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  const onChange = (e) => {
+    dispatch({ type: 'SET_INPUT', name: e.target.name, value: e.target.value });
+  }
 
   useEffect(() => {
     fetchNotes()
@@ -65,6 +97,26 @@ const App = () => {
 
   return (
     <div style={styles.container}>
+      <Input
+        onChange={onChange}
+        value={state.form.name}
+        placeholder="Note Name"
+        name='name'
+        style={styles.input}
+      />
+      <Input
+        onChange={onChange}
+        value={state.form.description}
+        placeholder="Note description"
+        name='description'
+        style={styles.input}
+      />
+      <Button
+        onClick={createNote}
+        type="primary"
+      >
+        Create Note
+      </Button>
       <List
         loading={state.loading}
         dataSource={state.notes}
